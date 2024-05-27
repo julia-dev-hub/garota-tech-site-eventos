a8694795073e72d7d84e2f895299915e2ff114a7
julia-dev-hub <147200410+julia-dev-hub@users.noreply.github.com>
2024-05-27 13:10:42 -0300

    Update and rename script.js to exercicios.js

---
+++ exercicios.js
@@ -0,0 +1,86 @@
+// Exercício 1 - Função e variáveis:
+function receitaDeBolo() {
+  // Seu código aqui
+}
+
+// Exercício 2 - if, else:
+function identificaIdade(idade){
+    // Seu código aqui
+}
+
+// Exercício 3 - Objetos e Arrays:
+let evento = {
+    id: 1,
+    titulo: "Festa da Firma",
+    data: "13/12/2021",
+    endereco: 'Rua dos Bobos, 0',
+};
+
+let listaDeEventos = [
+    evento,
+    // Seu código aqui
+];
+
+// Exercício 4 - Eventos:
+function confirmarPresenca(){
+     // Seu código aqui
+}
+
+// Exercício 5 - DOM (Document Object Model):
+const titulo = document.querySelector('.titulo-pagina');
+titulo.style.color = "";
+ 
+// Seu código aqui
+
+
+
+/* 
+
+Obs.: Importar esse arquivo no index.html
+
+Exercício 1 - Função e variáveis:
+
+- Crie uma função chamada receitaDeBolo que recebe vai receber 4 parâmentros: ingrediente1, ingrediente2, ingrediente3 e ingrediente4;
+
+- Dentro da função, crie as variáveis (var) passo1 e passo2 com as seguintes strings:
+`Bata ${ingrediente1} com ${ingrediente4}`;
+`Acrescente ${ingrediente2} e ${ingrediente3}`;
+
+- Crie a variável (let) receita e atribua a ela a seguinte string:
+`${passo1} \n${passo2} \nMisture bem.`;
+
+- Por fim, exiba um alert com a variável receita.
+
+
+Exercício 2 - if, else:
+
+- Crie uma função chamada identificaIdade que recebe o parâmetro idade;
+
+- Dentro dessa função crie uma estrutura condicional que verifica se a idade é maior ou igual a 18;
+
+- Se for maior, exiba um alert com a mensagem "Você é maior de idade". Caso contrário, exiba um alert com a mensagem "Você é menor de idade".
+
+
+Exercício 3 - Objetos e Arrays:
+
+- Imprima o objeto evento no console.
+- Imprima o valor da propriedade data do objeto evento no console.
+- Altere o valor da propriedade data do objeto evento para "20/07/2024".
+- Exiba o objeto no console novamente.
+- Crie um array chamado "listaDeEventos" que contém 3 objetos, cada um representando um evento diferente.
+- Imprima o primeiro item da lista de eventos no console.
+
+
+Exercício 4 - Eventos:
+
+- Crie uma função "confirmarPresenca" e adicione no método onclick do botão "Confirmar presença";
+- Dentro dessa função, exiba um alert com a mensagem "Presença confirmada no evento: Festa de Aniversário".
+
+
+Exercício 5 - DOM (Document Object Model):
+
+- Obtenha o elemento h1 a partir da sua class e atribua a cor "#01602A".
+- Obtenha o elemento header a partir da sua class e atribua a cor "#7DDF20".
+
+
+*/
